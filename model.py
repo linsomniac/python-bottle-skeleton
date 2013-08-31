039c1fffd6056f78c394d7ad9f40617862a800c4
Sean Reifschneider <jafo@tummy.com>
2013-08-31 15:51:23 -0600

    A bunch of sample updates.

--- model.py
+++ model.py
@@ -1,5 +1,64 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2013, Sean Reifschneider, tummy.com, ltd.
+#  All Rights Reserved.
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy import event, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.exc import DisconnectionError
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.pool import Pool
+import datetime
+from bottledbwrap import dbwrap
+import model
 
 def initdb():
     dbwrap.connect()
     dbwrap.Base.metadata.create_all()
     model.create_sample_data(dbwrap.session())
+
+def create_sample_current_oncall():
+    dbwrap.connect()
+    dbwrap.Base.metadata.create_all()
+    db = dbwrap.session()
+    model.create_sample_data(db)
+
+    sean = model.User(
+            full_name='Sean Reifschneider', username='sean',
+            email_address='jafo@example.com')
+    db.add(sean)
+    evi = model.User(
+            full_name='Evi Nemeth', username='evi',
+            email_address='evi@example.com')
+    db.add(evi)
+    dmr = model.User(
+            full_name='Dennis Ritchie', username='dmr',
+            email_address='dmr@example.com')
+    db.add(dmr)
+
+    db.commit()
+
+@event.listens_for(Pool, 'checkout')
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    '''Ping a database connection before using it to make sure it is still
+    alive.'''
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute('SELECT 1')
+    except:
+        # optional - dispose the whole pool instead of invalidating separately
+        # connection_proxy._pool.dispose()
+
+        # pool will try connecting again up to three times before raising.
+        raise DisconnectionError()
+    cursor.close()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer(), primary_key=True)
+
+    username = Column(String(length=20), nullable=False)
+    email_address = Column(String(length=60), nullable=False)
+    crypted_password = Column(String(length=200))
--- website.py
+++ website.py
@@ -3,7 +3,7 @@
 #  Copyright (c) 2013, Sean Reifschneider, tummy.com, ltd.
 #  All Rights Reserved.
 
-from tummydbwrap import dbwrap
+from bottledbwrap import dbwrap
 import model
 from bottle import (view, debug, TEMPLATE_PATH, Bottle, static_file, request,
         redirect)
@@ -21,40 +21,70 @@ def web_interface():
     return app
 
 
+#  Form validation example
+class NewUserFormProcessor(Form):
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email_address = TextField(
+            'Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField(
+            'New Password',
+            [validators.Required(),
+                validators.EqualTo('confirm',
+                    message='Passwords must match')
+            ])
+    confirm = PasswordField('Repeat Password')
+
 def routes(app):
     #  XXX Define application routes in this class
     @app.route('/<path:re:favicon.ico>')
     @app.route('/static/<path:path>')
     def static(path):
+        'Serve static content.'
         return static_file(path, root='static/')
 
     @app.route('/')
     @view('index')
     def index():
+        'A simple form that shows the date'
+
         import datetime
 
         date = datetime.datetime.now()
 
         return locals()
 
-    @app.route('/form')
-    @view('form')
-    def form():
+    @app.route('/users')
+    @view('users')
+    def index():
+        'A simple page from a dabase.'
+
         db = dbwrap.session()
 
-        class FormProcessor(Form):
-            username = TextField('Username', [validators.Length(min=4, max=25)])
-            email_address = TextField(
-                    'Email Address', [validators.Length(min=6, max=35)])
-            password = PasswordField(
-                    'New Password',
-                    [validators.Required(),
-                        validators.EqualTo('confirm',
-                            message='Passwords must match')
-                    ])
-            confirm = PasswordField('Repeat Password')
+        users = db.query(model.User).order_by(model.User.username)
 
-        form = FormProcessor(request.forms.decode())
+        return locals()
+
+    @app.route('/form')
+    @view('form')
+    def static_form():
+        'A simple form processing example'
+
+        form = NewUserFormProcessor(request.forms.decode())
+        if request.method == 'POST' and form.validate():
+            #  XXX Do something with form fields here
+
+            #  if successful
+            redirect('/users/%s' % form.username.data)
+        return locals()
+
+    @app.route('/database_form')
+    @view('form')
+    def database_form():
+        'A sample of interacting with a form and a dabase.'
+
+        db = dbwrap.session()
+
+        form = NewUserFormProcessor(request.forms.decode())
         if request.method == 'POST' and form.validate():
             #  XXX Do something with form fields here
 
